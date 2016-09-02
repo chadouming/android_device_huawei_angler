0b72f61f717f8ac1b8a7cc38bfa9689947bd5d8b
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-09-02 19:18:05 +0200

    Add f2fs support aka f2fs flag , build kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,10 +27,10 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 
 # prebuilt kernel
-TARGET_PREBUILT_KERNEL := device/huawei/angler/kernel
+# TARGET_PREBUILT_KERNEL := device/huawei/angler/kernel
 # else uncomment below to build from sauce
-# TARGET_KERNEL_SOURCE := kernel/huawei/angler
-# TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/angler
+TARGET_KERNEL_CONFIG := kylo_defconfig
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 41943040
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 41943040
@@ -40,6 +40,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
